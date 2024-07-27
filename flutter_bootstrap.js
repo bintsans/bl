9a884a45646cba7b40f8af01dfd0ceda9da366ea
bintsans <122976891+bintsans@users.noreply.github.com>
2024-07-27 22:05:42 +0200

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1845657710"
+    serviceWorkerVersion: "3399290348"
   }
 });
