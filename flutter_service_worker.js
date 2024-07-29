2cd8667e04399610b791bd764db8d9531795e8f7
bintsans <122976891+bintsans@users.noreply.github.com>
2024-07-29 17:29:48 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "893b50e719eb5ec977ea2c0086114ef8
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "eabc5bee3b00655e8c7fd9ee77a70e98",
+"flutter_bootstrap.js": "81882cdd47da4385cd4d3562e5ba3be1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
